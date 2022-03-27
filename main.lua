9daa8c00fc3ef5c82939c4f78b7dd1079854d5d3
wolfboyft <henryfthomson@gmail.com>
2022-03-27 11:23:34 +0100

    fixed shellFiller

--- main.lua
+++ main.lua
@@ -1,35 +1,45 @@
 -- TODO: come up with name for mod (and change key for event listeners)
 
--- NOTE: Currently projectileManager and shellFiller change item subtypes of ammo casings according to the [CONVERT_TO_(UN)FIREABLE:...] token. If projectiles and non-fireable casings change item type from ammo to tool, that code will have to change
+-- NOTE: Currently projectileManager and shellFiller change item subtypes of ammo casings according to the [CONVERT_TO_(UN)FIREABLE:...] token and also checks against segfaults in shellFiller by checking if job_item_refs are of type AMMO. If projectiles and non-fireable casings change item type from ammo to tool, that code will have to change
 -- NOTE: Adding the reactions doesn't seem to add anything undesirable like the shell filling dummy products to the civilisation's resources, which is good, but is all this certain?
 
 local eventful = require("plugins.eventful")
+
 local eventfulKey = "gunMod"
 local args = {...}
+
 if args[1] == "enable" then
+	-- Proper casing firing behaviour
 	local projectileManager = dfhack.run_script("gunMod/projectileManager")
 	eventful.onProjItemCheckMovement[eventfulKey] = function(...)
 		projectileManager(...)
 	end
+	
+	-- Code for filling casings
 	local shellFiller = dfhack.run_script("gunMod/shellFiller")
-	eventful.onReactionComplete[eventfulKey] = function(...)
-		-- shellFiller(...)
+	eventful.onJobCompleted[eventfulKey] = function(...)
+		shellFiller(...)
 	end
+	eventful.enableEvent(eventful.eventType.JOB_COMPLETED, 0)
+	
 	local itemCreationManager = dfhack.run_script("gunMod/itemCreationManager")
 	eventful.onItemCreated[eventfulKey] = function(...)
 		itemCreationManager(...)
 	end
+	
+	-- Cause extra havoc when a bullet is lodged firmly in a wound
 	local stuckInDamage = dfhack.run_script("gunMod/stuckInDamage")
 	eventful.onItemContaminateWound[eventfulKey] = function(...)
 		stuckInDamage(...)
 	end
-	print("Gun mod started!")
+	
+	print("Gun mod enabled!")
 elseif args[1] == "disable" then
 	eventful.onProjItemCheckMovement[eventfulKey] = nil
-	eventful.onReactionComplete[eventfulKey] = nil
+	eventful.onJobCompleted[eventfulKey] = nil
 	eventful.onItemCreated[eventfulKey] = nil
 	eventful.onItemContaminateWound[eventfulKey] = nil
-	print("Gun mod stopped. Behaviour may break.")
+	print("Gun mod disabled. Behaviour may break.")
 else
 	dfhack.printerr("Unknown argument \"" .. args[1] .. "\" to gunMod/main")
 end
--- shellFiller.lua
+++ shellFiller.lua
@@ -1,107 +1,121 @@
--- local customRawTokens = require("custom-raw-tokens")
--- 
--- -- This is an onReactionComplete event listener
--- local function shellFiller(reaction, reactionProduct, unit, inputItems, inputReagents, outputItems, callNative)
--- 	if not customRawTokens.getToken(reaction, "SHELL_FILLING_REACTION") then
--- 		return
--- 	end
--- 
--- 	-- TEMP: Pellets/shells refer to bullets/bullet casings too
--- 
--- 	local shell
--- 	do -- Process input items (ie limit reagents to intended amount and get shell)
--- 		local largestStack, totalPellets, desiredPellets
--- 
--- 		local pelletReagent = reaction.reagents[0]
--- 		desiredPellets = pelletReagent.quantity
--- 
--- 		totalPellets = 0
--- 		for i = 0, #inputItems - 1 do
--- 			local input = inputItems[i]
--- 			-- NOTE: If projectiles no longer have to be ammo, then add use a custom raw tag
--- 			if input._type == df.item_ammost then -- if pellet._type ~= df.item_ammost then continue end >:(
--- 				if customRawTokens.getToken(input.subtype, "AMMO_SHELL") then
--- 					assert(not shell)
--- 					shell = input
--- 
--- 					if shell.stack_size > 1 then
--- 						shell:splitStack(shell.stack_size - 1, true)
--- 					end
--- 				else -- A pellet stack, then
--- 					largestStack = largestStack or input
--- 					largestStack = input.stack_size > largestStack.stack_size and input or largestStack
--- 					totalPellets = totalPellets + input.stack_size
--- 				end
--- 			end
--- 		end
--- 		local excessPellets = totalPellets - desiredPellets
--- 		if excessPellets > 0 then
--- 			local rejectedStack = largestStack:splitStack(excessPellets, true)
--- 		end
--- 
--- 		-- Change subtype to fireable ammunition
--- 		local newSubtypeName = customRawTokens.getToken(shell.subtype, "CONVERT_TO_FIREABLE", true)
--- 		local defs = df.global.world.raws.itemdefs.ammo
--- 		for i = 0, #defs - 1 do
--- 			local itemDef = defs[i]
--- 			if itemDef.id == newSubtypeName then
--- 				shell:setSubtype(i)
--- 			end
--- 		end
--- 		shell:calculateWeight()
--- 	end
--- 
--- 	-- Move projectiles into shell
--- 	for i = 0, #inputItems - 1 do
--- 		local pellet = inputItems[i]
--- 		if pellet._type == df.item_ammost and not customRawTokens.getToken(pellet.subtype, "AMMO_SHELL") then
--- 			-- No specific refs (all prevent moveToContainer)
--- 			local j = 0
--- 			while #pellet.specific_refs < j do
--- 				local ref = pellet.specific_refs[j]
--- 				if ref.type == df.specific_ref_type.JOB then
--- 					pellet.specific_refs:erase(i)
--- 				else
--- 					error("Specific ref (prevents moveToContainer) found on pellet. Type: " .. df.specific_ref_type[ref.type])
--- 					j = j + 1
--- 				end
--- 			end
--- 
--- 			-- No general refs that prevent moveToContainer
--- 			local j = 0
--- 			local buildingId
--- 			while #pellet.general_refs < j do
--- 				local ref = pellet.general_refs[j]
--- 				local erase = true
--- 				if ref:getType() == df.general_ref_type.BUILDING_HELD then
--- 					buildingId = pellet.general_refs[i].building_id
--- 				elseif ref:getType() == df.general_ref_type.BUILDING_CAGED then
--- 				elseif ref:getType() == df.general_ref_type.BUILDING_TRIGGER then
--- 				elseif ref:getType() == df.general_ref_type.BUILDING_TRIGGERTARGET then
--- 				elseif ref:getType() == df.general_ref_type.BUILDING_CIVZONE_ASSIGNED then
--- 				else
--- 					erase = false
--- 				end
--- 				if erase then
--- 					pellet.general_refs:erase(i)
--- 				else
--- 					j = j + 1
--- 				end
--- 			end
--- 
--- 			if buildingId then
--- 				local building = df.building.find(buildingId)
--- 				for i = 0, #building.contained_items - 1 do
--- 					if building.contained_items[i].item == pellet then
--- 						building.contained_items:erase(i)
--- 						break
--- 					end
--- 				end
--- 			end
--- 
--- 			assert(dfhack.items.moveToContainer(pellet, shell), "Could not move projectile into shell!!")
--- 		end
--- 	end
--- end
--- 
--- return shellFiller
+local customRawTokens = require("custom-raw-tokens")
+local repeatUtil = require("repeat-util")
+
+local disconnect_clutter
+
+-- This is an onJobCompleted event listener
+local function shellFiller(job)
+	assert(job.completion_timer == 0, job.completion_timer)
+	if not job.reaction_name then return end
+	if job.reaction_name == "" then return end
+	if job.job_type ~= df.job_type.CustomReaction then return end
+	local reaction, reactionNumber
+	for i, reactionRaw in ipairs(df.global.world.raws.reactions.reactions) do
+		if job.reaction_name == reactionRaw.code then
+			reaction, reactionNumber = reactionRaw, i
+			break
+		end
+	end
+	if not reaction then return end
+	if not customRawTokens.getToken(reaction, "SHELL_FILLING_REACTION") then return end
+	
+	local shellStack
+	local freeProjectileStackPool = {}
+	for _, jobItemRef in ipairs(job.items) do
+		if jobItemRef.role == 1 then -- role 1 is reagent
+			if job.job_items[jobItemRef.job_item_idx].item_type == df.item_type.AMMO then
+				local itemStack = jobItemRef.item
+				if customRawTokens.getToken(itemStack, "AMMO_SHELL") then
+					assert(not shellStack)
+					shellStack = itemStack
+				elseif df.is_instance(df.item_ammost, itemStack) then
+					table.insert(freeProjectileStackPool, itemStack)
+				end
+			end
+		end
+	end
+	disconnect_clutter(shellStack)
+	if shellStack.stack_size > 1 then
+		shellStack = shellStack:splitStack(1, true)
+	end
+	
+	local newSubtypeName = customRawTokens.getToken(shellStack, "CONVERT_TO_FIREABLE")
+	local ammoItemDefs = df.global.world.raws.itemdefs.ammo
+	for i = 0, #ammoItemDefs - 1 do
+			local itemDef = ammoItemDefs[i]
+		if itemDef.id == newSubtypeName then
+			shellStack:setSubtype(i)
+		end
+	end
+	shellStack:calculateWeight()
+	
+	local projectileStacksToMove = {}
+	for _, reagent in ipairs(reaction.reagents) do
+		if reagent.item_type == df.item_type.AMMO and not customRawTokens.getToken(ammoItemDefs[reagent.item_subtype], "AMMO_SHELL") then
+			local amountRequired, amountAcquired = reagent.quantity, 0
+			local i = 1
+			while i <= #freeProjectileStackPool and amountAcquired < amountRequired do
+				local next = true
+				local stack = freeProjectileStackPool[i]
+				if reagent:matchesRoot(stack, reactionNumber) then
+					disconnect_clutter(stack)
+					local amountToTake = math.min(stack.stack_size, amountRequired - amountAcquired)
+					if amountToTake == stack.stack_size then
+						assert(stack == table.remove(freeProjectileStackPool, i))
+						table.insert(projectileStacksToMove, stack)
+						next = false
+					else
+						table.insert(projectileStacksToMove, stack:splitStack(amountToTake, true))
+					end
+					amountAcquired = amountAcquired + amountToTake
+				end
+				if next then
+					i = i + 1
+				end
+			end
+			assert(amountAcquired == amountRequired, "Amount acquired, amount required: " .. amountAcquired .. ", " .. amountRequired)
+		end
+	end
+	
+	for _, projectileStack in ipairs(projectileStacksToMove) do
+		disconnect_clutter(projectileStack)
+		dfhack.items.moveToContainer(projectileStack, shellStack)
+	end
+end
+
+function disconnect_clutter(item)
+    local bld = dfhack.items.getHolderBuilding(item)
+    if not bld then return true end
+    -- remove from contained items list, fail if not found
+    local found = false
+    for i,contained_item in ipairs(bld.contained_items) do
+        if contained_item.item == item then
+            bld.contained_items:erase(i)
+            found = true
+            break
+        end
+    end
+    if not found then
+        dfhack.printerr('failed to find clutter item in expected building')
+        return false
+    end
+    -- remove building ref from item and move item into containing map block
+    -- we do this manually instead of calling dfhack.items.moveToGround()
+    -- because that function will cowardly refuse to work with items with
+    -- BUILDING_HOLDER references (because it could crash the game). However,
+    -- we know that this particular setup is safe to work with.
+    for i,ref in ipairs(item.general_refs) do
+        if ref:getType() == df.general_ref_type.BUILDING_HOLDER then
+            item.general_refs:erase(i)
+            -- this call can return failure, but it always succeeds in setting
+            -- the required item flags and adding the item to the map block,
+            -- which is all we care about here. dfhack.items.moveToBuilding()
+            -- will fix things up later.
+            item:moveToGround(item.pos.x, item.pos.y, item.pos.z)
+            return true
+        end
+    end
+    return false
+end
+
+return shellFiller
