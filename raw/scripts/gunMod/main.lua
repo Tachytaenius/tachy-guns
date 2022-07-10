294353448e61d147e8811c49d4d46e1e6f9370e8
Tachytaenius <henryfthomson@gmail.com>
2022-07-10 14:00:42 +0100

    eventfulKey --> modId

--- raw/scripts/gunMod/main.lua
+++ raw/scripts/gunMod/main.lua
@@ -2,19 +2,19 @@
 
 local eventful = require("plugins.eventful")
 
-local eventfulKey = "gunMod"
+local modId = "gunMod"
 local args = {...}
 
 if args[1] == "enable" then
 	-- Proper casing firing behaviour
 	local projectileManager = dfhack.run_script("gunMod/projectileManager")
-	eventful.onProjItemCheckMovement[eventfulKey] = function(...)
+	eventful.onProjItemCheckMovement[modId] = function(...)
 		projectileManager(...)
 	end
 	
 	-- Making sawn-off shotguns
 	local typeTransform = dfhack.run_script("gunMod/typeTransform")
-	eventful.onJobCompleted[eventfulKey] = function(...)
+	eventful.onJobCompleted[modId] = function(...)
 		typeTransform(...)
 	end
 	eventful.enableEvent(eventful.eventType.JOB_COMPLETED, 0)
@@ -23,23 +23,23 @@ if args[1] == "enable" then
 	local handleMaterialTransfer = dfhack.run_script("gunMod/handleMaterialTransfer")
 	-- Storing projectile material from projectile bar in ammo product as an improvement
 	local storeProjectileMaterial = dfhack.run_script("gunMod/storeProjectileMaterial")
-	eventful.onReactionComplete[eventfulKey] = function(...)
+	eventful.onReactionComplete[modId] = function(...)
 		handleMaterialTransfer(...)
 		storeProjectileMaterial(...)
 	end
 	
 	-- Cause extra havoc when a bullet is lodged firmly in a wound
 	local stuckInDamage = dfhack.run_script("gunMod/stuckInDamage")
-	eventful.onItemContaminateWound[eventfulKey] = function(...)
+	eventful.onItemContaminateWound[modId] = function(...)
 		stuckInDamage(...)
 	end
 	
 	print("Gun mod enabled!")
 elseif args[1] == "disable" then
-	eventful.onProjItemCheckMovement[eventfulKey] = nil
-	eventful.onJobCompleted[eventfulKey] = nil
-	eventful.onReactionComplete[eventfulKey] = nil
-	eventful.onItemContaminateWound[eventfulKey] = nil
+	eventful.onProjItemCheckMovement[modId] = nil
+	eventful.onJobCompleted[modId] = nil
+	eventful.onReactionComplete[modId] = nil
+	eventful.onItemContaminateWound[modId] = nil
 	print("Gun mod disabled. Behaviour may break.")
 elseif not args[1] then
 	dfhack.printerr("No argument given to gunMod/main")
