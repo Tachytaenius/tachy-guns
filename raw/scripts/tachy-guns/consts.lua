eba12c9c6bbc58e19b3d14fa95de20d6b7f64929
Tachytaenius <henryfthomson@gmail.com>
2022-09-23 00:15:33 +0100

    Remove defaultFireExhaustion comment from consts

--- raw/scripts/tachy-guns/consts.lua
+++ raw/scripts/tachy-guns/consts.lua
@@ -5,9 +5,9 @@ dropCasingsAsItems = false -- (damaged) items or broken projectiles? TODO: setti
 perturbedVectorLength = 5000 -- Due to integer-only target locations
 skipProcessingProjectileFlagKey = 31
 
+
 -- game's own values
 gravity = 4900
--- consts.defaultFireExhaustion = 20 -- depends on attributes
 defaultFireExperienceGain = 30
 itemWearStep = 806400
 invalidCoord = -30000
